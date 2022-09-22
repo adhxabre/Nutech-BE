213c1035e5dfb2c21bbc2f4febeb61b830b61075
SodikulPadli <sodikulfadly91@gmail.com>
2022-09-22 08:43:02 +0800

    update config

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ const router = require('./src/routes');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
 app.use(cors());
-
+app.use(express.json());
 app.use('/api/v1/', router);
 app.use('/uploads', express.static('uploads'));
 app.get('/', function (req, res) {

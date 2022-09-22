9b77f1de5feaf22b33dfb6048421638e2a8f3579
SodikulPadli <sodikulfadly91@gmail.com>
2022-09-22 11:45:18 +0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+// dotenv
 require('dotenv').config();
+
+// express
 const express = require('express');
+
+// router
 const router = require('./src/routes');
+
+// cors
 const cors = require('cors');
 
 const app = express();
@@ -8,10 +15,12 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use('/api/v1/', router);
-// app.use('/uploads', express.static('uploads'));
+app.use('/api/v1', router);
+
 app.get('/', (req, res) => {
   res.send('Hello Developer!');
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}!`));
+app.listen(port, () => {
+  console.log(`Server is listenin on port ${port}`);
+});
